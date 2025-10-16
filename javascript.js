7bf4c2171566b491babf6c1298521e8deeb81850
Remill <tanglaoremill09@gmail.com>
2025-10-17 02:50:04 +0800

    js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
 /* Main JS */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+// Mobile menu toggle
+const menu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
